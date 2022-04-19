ea7bbdfb369e70a8e929d049d8b7a06ace2aa0eb
naokiwakata <rockwakata15@gmail.com>
2022-04-19 18:42:50 +0900

    Flaskの導入。画像を投げて植物を見つけることができた

--- main.py
+++ main.py
@@ -7,47 +7,64 @@ from detectron2.utils.logger import setup_logger
 from detectron2.data.datasets import register_coco_instances
 from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import Visualizer
+from flask import Flask, render_template, request
+import numpy as np
+import datetime
+
+app = Flask(__name__)
+
+# Initialize
+# データセットを登録
+register_coco_instances(
+    "leaf", {}, "PumpkinLeaf\PumpkinLeaf.json", "PumpkinLeaf/")
+coins_metadata = MetadataCatalog.get("leaf")
+setup_logger()
+
+# 設定を決める
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file(
+    "COCO-InstanceSegmentation\\mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 1クラスのみ
+
+cfg.MODEL.WEIGHTS = os.path.join(
+    cfg.OUTPUT_DIR, "C:\\Users\\wakanao\\detectron2_flask\\model_final.pth")  # 絶対パスでなければならないっぽい
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
+cfg.MODEL.DEVICE = "cpu"
+
+# 予測器を作成
+predictor = DefaultPredictor(cfg)
+img_dir = "static/imgs/"
 
 
-def main():
-    # データセットを登録
-    register_coco_instances(
-        "leaf", {}, "PumpkinLeaf\PumpkinLeaf.json", "PumpkinLeaf/")
-    coins_metadata = MetadataCatalog.get("leaf")
-    setup_logger()
+@app.route('/', methods=["GET", "POST"])
+def predict_img():
+    if request.method == 'GET':
+        img_path = None
+    elif request.method == 'POST':
 
-    # 設定を決める
-    cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file(
-        "COCO-InstanceSegmentation\\mask_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 1クラスのみ
+        # POSTにより受け取った画像を読み込む
+        stream = request.files['img'].stream
+        img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+        img = cv2.imdecode(img_array, 1)
+        # 現在時刻を名前として「imgs/」に保存する
 
-    cfg.MODEL.WEIGHTS = os.path.join(
-        cfg.OUTPUT_DIR, "C:\\Users\\wakanao\\detectron2_flask\\model_final.pth")  # 絶対パスでなければならないっぽい
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
-    cfg.MODEL.DEVICE = "cpu"
+        img = np.clip(img, 0, 255).astype(np.uint8)
 
-    # 予測器を作成
-    predictor = DefaultPredictor(cfg)
+        outputs = predictor(img)
 
-    # 予測および表示
-    imagePath = "images\\9_11 (2).JPG"
-    img = cv2.imread(imagePath)
+        dt_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+        img_path = img_dir + dt_now + ".jpg"
+        v = Visualizer(img[:, :, ::-1],
+                       metadata=coins_metadata,
+                       scale=1.0
+                       )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        img = v.get_image()[:, :, ::-1]
+        cv2.imwrite(img_path, img)
 
-    outputs = predictor(img)
-    print(outputs["instances"].pred_classes)  # クラス分類：1クラスなので０のみ
-    print(outputs["instances"].scores)  # 予測精度
-    print(outputs["instances"].pred_boxes)  # ボックス(4座標)
-    print(outputs["instances"].pred_masks)  # マスク(True,False)
-
-    v = Visualizer(img[:, :, ::-1],
-                   metadata=coins_metadata,
-                   scale=1.0
-                   )
-    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    cv2.imshow('image', v.get_image()[:, :, ::-1])
-    cv2.waitKey(0)
+    # 保存した画像ファイルのpathをHTMLに渡す
+    return render_template('index.html', img_path=img_path)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run()
