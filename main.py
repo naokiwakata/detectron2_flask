51b3b3ae8bfdb0b115d5b12422c4c312e7229415
naokiwakata <rockwakata15@gmail.com>
2022-04-25 15:04:45 +0900

    predictorクラスで動くように変更

--- main.py
+++ main.py
@@ -11,29 +11,11 @@ from flask import Flask, render_template, request
 import numpy as np
 import datetime
 
+from predictor import Predictor
+
 app = Flask(__name__)
 
-# Initialize
-# データセットを登録
-register_coco_instances(
-    "leaf", {}, "PumpkinLeaf\PumpkinLeaf.json", "PumpkinLeaf/")
-coins_metadata = MetadataCatalog.get("leaf")
-setup_logger()
-
-# 設定を決める
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file(
-    "COCO-InstanceSegmentation\\mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 1クラスのみ
-
-cfg.MODEL.WEIGHTS = os.path.join(
-    cfg.OUTPUT_DIR, "C:\\Users\\wakanao\\detectron2_flask\\model_final.pth")  # 絶対パスでなければならないっぽい
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
-cfg.MODEL.DEVICE = "cpu"
-
-# 予測器を作成
-predictor = DefaultPredictor(cfg)
-img_dir = "static/imgs/"
+predictor = Predictor()
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -50,16 +32,12 @@ def predict_img():
 
         img = np.clip(img, 0, 255).astype(np.uint8)
 
-        outputs = predictor(img)
+        predictor.predict(img=img)
+        img = predictor.img
 
         dt_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
-        img_path = img_dir + dt_now + ".jpg"
-        v = Visualizer(img[:, :, ::-1],
-                       metadata=coins_metadata,
-                       scale=1.0
-                       )
-        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        img = v.get_image()[:, :, ::-1]
+        _img_dir = "static/imgs/"
+        img_path = _img_dir + dt_now + ".jpg"
         cv2.imwrite(img_path, img)
 
     # 保存した画像ファイルのpathをHTMLに渡す
